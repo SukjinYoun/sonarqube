86c95df3cff5160f719978ebd64e8da9ca3eae55
Sukjin <40019779+SukjinYoun@users.noreply.github.com>
2024-09-27 21:02:42 +0900

    Update app.js

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+
 const app = express();
 const port = 3000;
+
+//let example = express();
+app.disable("x-powered-by");
 
 app.get('/', (req, res) => {
   let a = 1;
